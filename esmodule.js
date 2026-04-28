a978b2a7d7e19d01afb51ceef55afe6fcad89f4a
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-28 14:37:28 +1000

    esmodules default and named

---
+++ esmodule.js
@@ -0,0 +1,10 @@
+export const add = (a, b) => {
+  return a + b;
+};
+//named export
+export const sub = (a, b) => {
+  return a - b;
+};
+
+// export { add, sub }; => named export for exporting both at once
+// export default add;
---
+++ index.js
@@ -0,0 +1,17 @@
+//! module
+//common js
+
+// const { add, sub } = require("./math");
+// console.log(add);
+
+//esmodule
+// default import
+// import add from "./esmodule.js";
+//named import
+import { add, sub } from "./esmodule.js";
+console.log(add(12, 2));
+console.log(sub(12, 2));
+
+//types of module
+//common js =>require()
+//ESmodule => import/export
