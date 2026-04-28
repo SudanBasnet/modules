86a0609ab72e568caee6e48c0851c4bd6a9399cc
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-28 15:37:48 +1000

    JSON

--- index.js
+++ index.js
@@ -8,10 +8,24 @@
 // default import
 // import add from "./esmodule.js";
 //named import
-import { add, sub } from "./esmodule.js";
-console.log(add(12, 2));
-console.log(sub(12, 2));
+// import { add, sub } from "./esmodule.js";
+// console.log(add(12, 2));
+// console.log(sub(12, 2));
 
 //types of module
 //common js =>require()
 //ESmodule => import/export
+
+//JSON => js object notation
+
+let user = {
+  name: "sudan",
+  email: "sudan@gmail.com",
+};
+
+const json = JSON.stringify(user);
+
+console.log(user);
+console.log(json);
+
+console.log(JSON.parse(json).name);
